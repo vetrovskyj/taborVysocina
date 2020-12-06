26fa413e10a72f1d0007e4648e9c1e3f3b0dec5c
Jan Větrovský <vetj00@vse.cz>
2020-12-06 14:57:24 +0100

    hotove nacteni novinek

--- webVysocina_html_edice/js/paste.js
+++ webVysocina_html_edice/js/paste.js
@@ -1,20 +1,13 @@
-for (let i = 0; i < 4; i++) {
-    const content = document.createElement('div');
+const content = document.createElement('a');
+content.setAttribute('href', './Vylety.html');
+content.classList.add('content-item');
 
-    const contentHeadingArea = document.createElement('a');
-    contentHeadingArea.setAttribute('href', './Vylety.html');
-    contentHeadingArea.classList.add('content-item');
+const newsContent = document.querySelector('.news-content');
+$(content).load('./Vylety.html .vylet');
+newsContent.appendChild(content);
 
-    $(contentHeadingArea).load('./Vylety.html .vylet h1:first-child');
-    console.log(contentHeadingArea);
-    content.appendChild(contentHeadingArea);
-
-    const contentTextArea = document.createElement('p');
-    contentTextArea.classList.add('content-item-text');
-    $(contentTextArea).load('./Vylety.html .vylet p');
-    console.log(contentTextArea);
-    content.appendChild(contentTextArea);
-
-    const newsContent = document.querySelector('.news-content');
-    newsContent.appendChild(content);
-}
+/*const contentTextArea = document.createElement('p');
+contentTextArea.classList.add('content-item-text');
+$(contentTextArea).load('./Vylety.html .vylet p');
+console.log(contentTextArea);
+content.appendChild(contentTextArea);*/

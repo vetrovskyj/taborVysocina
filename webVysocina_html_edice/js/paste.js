e0a4d83d39ceededa46b9619cfc00dc7d300bd96
Jan Větrovský <vetj00@vse.cz>
2020-12-07 08:54:22 +0100

    zmena aktualit a omezeni pro tri aktuality na home

--- webVysocina_html_edice/js/paste.js
+++ webVysocina_html_edice/js/paste.js
@@ -1,13 +1,14 @@
-const content = document.createElement('a');
-content.setAttribute('href', './Vylety.html');
-content.classList.add('content-item');
+window.onload = function() {
+        const content = document.createElement('a');
+        content.setAttribute('href', './Vylety.html');
+        content.classList.add('content-item');
 
-const newsContent = document.querySelector('.news-content');
-$(content).load('./Vylety.html .vylet');
-newsContent.appendChild(content);
-
-/*const contentTextArea = document.createElement('p');
-contentTextArea.classList.add('content-item-text');
-$(contentTextArea).load('./Vylety.html .vylet p');
-console.log(contentTextArea);
-content.appendChild(contentTextArea);*/
+        const newsContent = document.querySelector('.news-content');
+        $(content).load('./Vylety.html .vylet:lt(3)');
+        newsContent.appendChild(content);
+    }
+    /*const contentTextArea = document.createElement('p');
+    contentTextArea.classList.add('content-item-text');
+    $(contentTextArea).load('./Vylety.html .vylet p');
+    console.log(contentTextArea);
+    content.appendChild(contentTextArea);*/

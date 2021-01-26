780328536cc0d308a2ac42c216782be682c6acf3
Jan Větrovský <vetj00@vse.cz>
2021-01-26 01:26:43 +0100

    articles responsive

---
+++ webVysocina_html_edice/js/articles.js
@@ -0,0 +1,6 @@
+const foto = document.querySelector('#foto-id');
+const text = $('.text-vyletu p');
+
+//text.css('max-height', `${foto[0].height()/3}`);
+console.log(foto);
+console.log(foto.clientHeight);

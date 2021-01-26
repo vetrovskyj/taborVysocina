70d3467d25e59178a60ad25b909375352000814a
Jan Větrovský <vetj00@vse.cz>
2021-01-26 19:19:43 +0100

    people change

---
+++ webVysocina_html_edice/js/lide.js
@@ -0,0 +1,21 @@
+const osoby = $('.osoba');
+const profilePhoto = $('.profile-description');
+
+
+let arr = [];
+for (let i = 0, len = osoby.length; i < len; i++) {
+    (function(index) {
+        osoby[i].onmouseover = function() {
+            setTimeout(() => {
+                profilePhoto[index].className = '';
+            }, 200);
+            osoby[index].classList.toggle('transition');
+        };
+        osoby[i].onmouseout = function() {
+            setTimeout(() => {
+                profilePhoto[index].className = 'profile-description';
+            }, 200);
+            osoby[index].className = 'osoba';
+        };
+    })(i);
+};

921325e2c12c09ed13385eadcd4288e089ee6ca5
Jan Větrovský <vetj00@vse.cz>
2021-01-29 01:40:12 +0100

    scrolls

--- js/lightbox.js
+++ js/lightbox.js
@@ -97,3 +97,56 @@ lightboxContainer.addEventListener('click', e => {
     prev.classList.remove('active');
     next.classList.remove('active');
 });
+
+lightbox.addEventListener('touchstart', handleTouchStart, false);
+lightbox.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches || // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
+        if (xDiff > 0) {
+            img.src = nextItem().src;
+            while (lightbox.firstChild) {
+                lightbox.removeChild(lightbox.firstChild);
+            };
+            lightbox.appendChild(img);
+            lightbox.appendChild(prev);
+            lightbox.appendChild(next);
+        } else {
+            img.src = prevItem().src;
+            while (lightbox.firstChild) {
+                lightbox.removeChild(lightbox.firstChild);
+            };
+            lightbox.appendChild(img);
+            lightbox.appendChild(prev);
+            lightbox.appendChild(next);
+        }
+
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
